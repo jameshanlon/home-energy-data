138c326cf9605af8add9056482f1b1c9111ceaa8
James Hanlon <mail@jameswhanlon.com>
2024-10-04 13:27:46 +0000

    Calculate statistics

--- analyse-energy-data.py
+++ analyse-energy-data.py
@@ -17,6 +17,7 @@ from dataclasses import dataclass
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 from collections import defaultdict
+from typing import Iterator
 
 
 class Record:
@@ -54,10 +55,9 @@ class Chart:
         return self.name.lower().replace(" ", "_").replace("(", "_").replace(")", "_")
 
 
-@dataclass
 class Stats:
-    annual_cop_2023: float
-    annual_cop_2024: float
+    def __init__(self, year: int):
+        self.year = year
 
 
 class Dataset:
@@ -72,6 +72,17 @@ class Dataset:
         attr_name = metric.replace(":", "_")
         assert getattr(self.records[date], attr_name) == None, "overwriting data point"
         setattr(self.records[date], attr_name, value)
+
+    def iter_year(self, year: int) -> Iterator[Record]:
+        for record in self.records.values():
+            if record.DateTime.year == year:
+                yield record
+
+    def total(self, year: int, metric: str):
+        return sum(
+            getattr(record, metric) if getattr(record, metric) != None else 0
+            for record in self.iter_year(year)
+        )
 
     def dump(self):
         metrics = [
@@ -112,7 +123,7 @@ def read_csv(dataset, filename: str, headers: list[str]) -> Dataset:
         logging.info(f"Read {count} rows from {filename}")
 
 
-def generate_html(charts: list[Chart], output_path: Path):
+def generate_html(charts: list[Chart], annual_stats: list[Stats], output_path: Path):
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("index.html")
     content = template.render(charts=charts)
@@ -258,12 +269,35 @@ def main(args):
             chart.add_datapoint("External", record.OutdoorTemperature)
     charts.append(chart)
 
+    # TODO: heat output vs COP
+
+    # Prepare stats.
+    annual_stats = []
+    for year in [2023, 2024]:
+        s = Stats(year)
+        s.annual_heating_consumed = dataset.total(
+            year, "ConsumedElectricalEnergy_Heating"
+        )
+        s.annual_water_consumed = dataset.total(
+            year, "ConsumedElectricalEnergy_DomesticHotWater"
+        )
+        s.annual_total_consumed = s.annual_heating_consumed + s.annual_water_consumed
+        s.annual_heating_generated = dataset.total(year, "HeatGenerated_Heating")
+        s.annual_water_generated = dataset.total(year, "HeatGenerated_Heating")
+        s.annual_total_generated = s.annual_heating_consumed + s.annual_water_consumed
+        s.heating_scop = s.annual_heating_generated / s.annual_heating_consumed
+        s.water_scop = s.annual_water_generated / s.annual_water_consumed
+        s.scop = (s.annual_heating_generated + s.annual_water_generated) / (
+            s.annual_heating_consumed + s.annual_water_consumed
+        )
+        annual_stats.append(s)
+
     # Output path.
     output_path = Path(args.output_dir)
     output_path.mkdir(exist_ok=True)
 
     # Generate HTML
-    generate_html(charts, output_path)
+    generate_html(charts, annual_stats, output_path)
 
 
 if __name__ == "__main__":
